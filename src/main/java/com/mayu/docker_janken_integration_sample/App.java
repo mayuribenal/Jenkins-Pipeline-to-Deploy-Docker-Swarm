9d723594ad694d656cf71a45b8417a164668866f
mayuribenal <mayuribenal111@gmail.com>
2022-10-10 02:01:56 +0530

    file add

---
+++ src/main/java/com/mayu/docker_janken_integration_sample/App.java
@@ -0,0 +1,13 @@
+package com.mayu.docker_janken_integration_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
